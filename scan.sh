ea54d7318138f1851827216fbfae1d7839120af9
root <ahmedx4123@hotmail.com>
2023-11-19 14:47:45 +0000

    tested_code

---
+++ scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCANNER_PATH=`pwd`
+echo $SCANNER_PATH
+RULES="scanner_code/scanner_rules"
+WRAPPER="scanner_code/scanner_wrapper"
+CODE_TO_SCAN="/root/code"
+python3 $SCANNER_PATH/$WRAPPER/scan.py $CODE_TO_SCAN $SCANNER_PATH/$RULES
+python3 $SCANNER_PATH/$WRAPPER/json_to_html.py $SCANNER_PATH/output.json  /var/www/html/
+
