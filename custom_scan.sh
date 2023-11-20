42e576745714dbf94cfdb19f92ec9779553e6f1a
root <ahmedx4123@hotmail.com>
2023-11-20 18:25:06 +0000

    new custom rules

---
+++ custom_scan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCANNER_PATH=`pwd`
+echo $SCANNER_PATH
+RULES="scanner_code/scanner_rules"
+WRAPPER="scanner_code/scanner_wrapper"
+CODE_TO_SCAN="/root/code"
+MY_RULES="custom_rules"
+python3 $SCANNER_PATH/$WRAPPER/scan.py $CODE_TO_SCAN $SCANNER_PATH/$RULES/$MY_RULES
+python3 $SCANNER_PATH/$WRAPPER/json_to_html.py $SCANNER_PATH/output.json  /var/www/html/
+
