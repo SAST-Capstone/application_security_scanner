10c2c1daa6b22cd76a2c3076da0d06e89b82f95e
PortgasQaddoura <144597269+PortgasQaddoura@users.noreply.github.com>
2024-06-20 23:54:31 +0400

    Update sast_scan.sh

--- sast_scan.sh
+++ sast_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCANNER_PATH=`pwd`
+SCANNER_PATH=$(pwd)
 echo $SCANNER_PATH
 RULES="scanner_code/scanner_rules"
 WRAPPER="scanner_code/scanner_wrapper"
@@ -12,4 +12,10 @@ REPO_NAME=$(basename $GITHUB_REPOSITORY)
 CODE_TO_SCAN="/home/runner/work/$REPO_NAME/$REPO_NAME"
 
 MY_RULES="custom_rules"
+
+# Run the scan
 python3 $SCANNER_PATH/$WRAPPER/sast_scan.py $CODE_TO_SCAN $SCANNER_PATH/$RULES/$MY_RULES
+
+# Send the results to the database
+python3 $SCANNER_PATH/$WRAPPER/send_results.py $SCANNER_PATH/$WRAPPER/scan_results.json
+
