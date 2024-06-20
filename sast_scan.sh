3c174107a46e9780b512f9cbd2003cfb7a25be92
PortgasQaddoura <144597269+PortgasQaddoura@users.noreply.github.com>
2024-06-21 00:24:59 +0400

    Update sast_scan.sh

--- sast_scan.sh
+++ sast_scan.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 SCANNER_PATH=$(pwd)
-echo "Scanner Path: $SCANNER_PATH"
-RULES="$SCANNER_PATH/scanner_code/scanner_rules"
-WRAPPER="$SCANNER_PATH/scanner_code/scanner_wrapper"
+echo $SCANNER_PATH
+RULES="scanner_code/scanner_rules"
+WRAPPER="scanner_code/scanner_wrapper"
 
 # Extract the repository name from the GITHUB_REPOSITORY environment variable
 REPO_NAME=$(basename $GITHUB_REPOSITORY)
-echo "Repository Name: $REPO_NAME"
 
 # Set the CODE_TO_SCAN path dynamically
 CODE_TO_SCAN="/home/runner/work/$REPO_NAME/$REPO_NAME"
-echo "Code to Scan: $CODE_TO_SCAN"
 
 MY_RULES="custom_rules"
 
 # Run the scan
-python3 "$WRAPPER/sast_scan.py" "$CODE_TO_SCAN" "$RULES/$MY_RULES"
+python3 $SCANNER_PATH/$WRAPPER/sast_scan.py $CODE_TO_SCAN $SCANNER_PATH/$RULES/$MY_RULES
 
 # Send the results to the database
-python3 "$WRAPPER/send_results.py" "$WRAPPER/scan_results.json"
+python3 $SCANNER_PATH/$WRAPPER/send_results.py $SCANNER_PATH/$WRAPPER/scan_results.json
+
+
+
